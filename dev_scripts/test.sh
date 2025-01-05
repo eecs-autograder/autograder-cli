90e39638849a2d36c10201cc799bc5452a6785b8
James Perretta <jameslp@umich.edu>
2025-01-05 14:43:40 -0500

    Working on CI: - Updated tests to generate and use a fake agtoken file

--- dev_scripts/test.sh
+++ dev_scripts/test.sh
@@ -4,4 +4,5 @@ set -e
 
 script_dir=$(dirname "$(realpath $0)")
 python $script_dir/../tests/roundtrip/setup_db.py
+echo "fake token" > tests.agtoken
 pytest -n auto --tb=short --ignore=$script_dir/../tests/local_stack $@
