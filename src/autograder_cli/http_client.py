bc94ae79757e8aa798e25dfedd1ae3e55d80cd19
James Perretta <jameslp@umich.edu>
2025-08-06 11:51:12 -0400

    Add get_file subcommand command to http client CLI. (#18)

--- src/autograder_cli/http_client.py
+++ src/autograder_cli/http_client.py
@@ -116,12 +116,10 @@ class HTTPClient:
 
 def check_response_status(response: requests.Response):
     if not response.ok:
-        if 500 <= response.status_code < 600:
-            print(f"{response.status_code}: {response.reason}")
-        else:
-            try:
-                print(response.json())
-            except ValueError:
-                print(response.text)
+        print(f"{response.status_code}: {response.reason}")
+        try:
+            print(response.json())
+        except ValueError:
+            print(response.text)
 
-        response.raise_for_status()
+    response.raise_for_status()
